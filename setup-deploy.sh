02d814ff695108d0772091a70e8087b5aaeaf855
Tu Nombre <you@example.com>
2018-05-09 12:19:18 +0200

    rutas absolutas

--- setup-deploy.sh
+++ setup-deploy.sh
@@ -12,14 +12,14 @@ sudo chmod -R 777 /var/www/$carpeta_proyecto
 project_name=$carpeta_proyecto
 dns=`/home/ubuntu/script_php/json-bash.sh DNS $project_name | cut -d" " -f2`
 site_apache=`/home/ubuntu/script_php/json-bash.sh SITE_APACHE $project_name | cut -d" " -f2`
-cp template-site.conf template-site.conf.tmp
-sed -i 's/$PROJECT_NAME/'$project_name'/g' template-site.conf.tmp
-sed -i 's/$SERVER_NAME/'$dns'/g' template-site.conf.tmp
-sed -i 's/$ERROR_LOG/'$project_name'_error.log/g' template-site.conf.tmp
-sed -i 's/$ACCESS_LOG/'$project_name'_access.log/g' template-site.conf.tmp
+cp /home/ubuntu/script_php/template-site.conf /home/ubuntu/script_php/template-site.conf.tmp
+sed -i 's/$PROJECT_NAME/'$project_name'/g' /home/ubuntu/script_php/template-site.conf.tmp
+sed -i 's/$SERVER_NAME/'$dns'/g' /home/ubuntu/script_php/template-site.conf.tmp
+sed -i 's/$ERROR_LOG/'$project_name'_error.log/g' /home/ubuntu/script_php/template-site.conf.tmp
+sed -i 's/$ACCESS_LOG/'$project_name'_access.log/g' /home/ubuntu/script_php/template-site.conf.tmp
 if [ ! -f /etc/apache2/sites-enabled/$site_apache ];
 then
-	sudo cp template-site.conf.tmp /etc/apache2/sites-enabled/$site_apache
+	sudo cp /home/ubuntu/script_php/template-site.conf.tmp /etc/apache2/sites-enabled/$site_apache
 fi
 sudo a2ensite $site_apache
 sudo systemctl restart apache2
